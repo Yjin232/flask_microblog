76811b7b3b183fee519dc18baba98849d96f3ef0
Yjin232 <jyicheng718@gmail.com>
2021-12-28 15:44:48 -0800

    templates finished

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,24 @@
 #Author: Yicheng Jin
 #Date: 12/28/2021
-
+from flask import request,render_template,redirect
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return 'Hello, World!'
+    #add user info
+    user = {'username':'Yicheng'}
+    #add user post,list type
+    posts = [
+        {
+            'author': {'username': 'Mike'},
+            'body': 'Beautiful day in Seattle!'
+         },
+        {
+            'author': {'username': 'Anna'},
+            'body': 'Beautiful day in Las Vegas!'
+        }
+    ]
+
+    return render_template('index.html',title='Home',user=user,posts=posts)
