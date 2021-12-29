1fe033c5af52545bf3b97e5decfafa052ee5d1db
Yjin232 <jyicheng718@gmail.com>
2021-12-28 20:18:56 -0800

    ch4--Database Finished

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,18 @@
 #Date: 12/28/2021
 from flask import Flask # import Flask lib
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+#this command to relaize all the config
 app.config.from_object(Config)
-# print(app.config['SECRET_KEY'])
+
+#init the database
+db = SQLAlchemy(app) # Database object
+migrate = Migrate(app,db) # Migrate object
 
 #import routes module from app
-from app import routes #import routes module from app
+from app import routes,models #import routes module from app
 
--- microblog.py
+++ microblog.py
@@ -1,6 +1,11 @@
 #Author: Yicheng Jin
 #Date: 12/28/2021
-from app import app
+from app import app, db
+from app.models import User, Post
+#shell上下文：进入flask.shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db,'User':User,'Post':Post}
 
 if __name__ == '__main__':
 
