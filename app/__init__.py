46f6bc98cf82d5dffbb880df160017718db9ef4a
Yjin232 <jyicheng718@gmail.com>
2021-12-31 20:31:28 -0800

    Ch10 Flask_bootstrap finished

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 #this command to relaize all the config
@@ -20,7 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
-
+bootstrap = Bootstrap(app)
 #import routes module from app
 from app import routes,models #import routes module from app
 
